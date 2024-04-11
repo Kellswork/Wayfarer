a7a626a30a3cccd7b90cce32197827f4621429c1
Kellswork <kellogbonna@gmail.com>
2024-04-11 11:05:04 +0100

    update createUser test and add login user test in the controller

--- internal/api/controllers/user_controller_test.go
+++ internal/api/controllers/user_controller_test.go
@@ -7,11 +7,13 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/kellswork/wayfarer/internal/db/models"
 	"github.com/kellswork/wayfarer/internal/db/repositories/mocks"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -53,10 +55,81 @@ func TestCreateUser(t *testing.T) {
 
 	req.Header.Set("Content-Type", "application/json")
 	recorder := httptest.NewRecorder()
-
 	r.ServeHTTP(recorder, req)
 
 	// assert the response
 	require.Equal(t, http.StatusCreated, recorder.Code)
+
+	result := struct {
+		Status string             `json:"status"`
+		Data   models.CreatedUser `json:"data"`
+	}{}
+	err = json.Unmarshal(recorder.Body.Bytes(), &result)
+	assert.Nil(t, err)
+	assert.Equal(t, "success", result.Status)
+	assert.False(t, result.Data.IsAdmin)
+
+}
+
+func TestLoginUser(t *testing.T) {
+
+	// create gin engine
+	r := gin.Default()
+
+	// initilaise mocks
+	ctrl := gomock.NewController(t)
+	mockUserRepo := mocks.NewMockUserRepository(ctrl)
+
+	// initialise controller with the mock repository
+	userctrl := NewUserControllers(mockUserRepo)
+
+	r.POST("/login", userctrl.LoginUser)
+
+	// create user request body
+	sampleUser := models.LoginUserRequest{
+		Email:    "kellssyy@gmail.com",
+		Password: "1234",
+	}
+
+	// check if email exists
+	ctx := context.Background()
+	mockUserRepo.EXPECT().EmailExists(ctx, sampleUser.Email).Return(true, nil)
+
+	// mock retuned user
+	mockReturnedUser := models.User{
+
+		ID:        "e12bab25-e10f-4920-ab57-5fa3dee389ae",
+		Email:     sampleUser.Email,
+		FirstName: "Kelechi",
+		LastName:  "Ogbonna",
+		Password:  "$2a$10$th3pXsOR66QBnzRiNlW7We67RfuKlZH2n11JtJ08e4mc7zsMeXXzu", // Hashed password
+		IsAdmin:   false,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Time{},
+	}
+	mockUserRepo.EXPECT().GetByEmail(ctx, sampleUser.Email).Return(&mockReturnedUser, nil)
+
+	sampleUserByte, err := json.Marshal(sampleUser)
+	require.NoError(t, err)
+
+	// perform HTTP request
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(sampleUserByte))
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
+	r.ServeHTTP(recorder, req)
+
+	// assert the response
+	require.Equal(t, http.StatusOK, recorder.Code)
+
+	result := struct {
+		Status string           `json:"status"`
+		Data   models.LoginUser `json:"data"`
+	}{}
+	err = json.Unmarshal(recorder.Body.Bytes(), &result)
+	assert.Nil(t, err)
+	assert.Equal(t, "success", result.Status)
+	assert.False(t, result.Data.IsAdmin)
 
 }
