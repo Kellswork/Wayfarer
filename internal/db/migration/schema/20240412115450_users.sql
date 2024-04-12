b90d57709a92563b329456f4af630afc32e40cb8
Kellswork <kellogbonna@gmail.com>
2024-04-12 13:45:43 +0100

    create bus table

--- internal/db/migration/schema/20240412115450_users.sql
+++ internal/db/migration/schema/20240412115450_users.sql
@@ -3,7 +3,7 @@
 CREATE TABLE
   IF NOT EXISTS users (
     id UUID PRIMARY KEY NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
