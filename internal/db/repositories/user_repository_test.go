5b1f50a39854d7585353882fe7ceedfcd45a2be0
Kellswork <kellogbonna@gmail.com>
2024-05-01 20:27:44 +0100

    update tests

--- internal/db/repositories/user_repository_test.go
+++ internal/db/repositories/user_repository_test.go
@@ -3,44 +3,65 @@ package repositories
 import (
 	"context"
 	"database/sql"
-	"os"
+	"log"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/joho/godotenv"
 	"github.com/kellswork/wayfarer/internal/db"
 	"github.com/kellswork/wayfarer/internal/db/migration"
 	"github.com/kellswork/wayfarer/internal/db/models"
 	"github.com/kellswork/wayfarer/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_CreateUser(t *testing.T) {
-	// 1. setup pahse
-	// 1.1 setup db connectiom
-	// err := godotenv.Load("../../../.env")
-	// require.NoError(t, err)
-	// testDBUrl := os.Getenv("TEST_DB_URL")
-	ctx := context.Background()
+type UserRepoTestSuite struct {
+	suite.Suite
+	pgContiner *utils.PostgresContainer
+	userRepo   *userRespository
+	ctx        context.Context
+	dbStore    *sql.DB
+}
 
-	container, err := utils.CreatePostgresContainer(ctx)
-	require.NoError(t, err)
+func (suite *UserRepoTestSuite) SetupSuite() {
+	suite.ctx = context.Background()
+	container, err := utils.CreatePostgresContainer(suite.ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	suite.pgContiner = container
 
-	t.Cleanup(func() {
-		if err := container.Terminate(ctx); err != nil {
-			t.Fatalf(" failed to terminate container: %s", err)
-		}
-	})
+	dbStore, err := db.ConnectDatabase(suite.pgContiner.ConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	dbStore, err := db.ConnectDatabase(container.ConnectionString)
-	require.NoError(t, err)
+	suite.dbStore = dbStore
 
 	migrator := migration.New(dbStore)
 	err = migrator.Up()
-	assert.NoError(t, err)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	userReporistory := newUserRepository(dbStore)
+	if err != nil {
+		log.Fatal(err)
+	}
+	suite.userRepo = userReporistory
+}
+
+func (suite *UserRepoTestSuite) TearDownSuite() {
+	if err := suite.pgContiner.Terminate(suite.ctx); err != nil {
+		log.Fatalf(" failed to terminate postgres container: %s", err)
+	}
+}
+
+func (suite *UserRepoTestSuite) Test_CreateUser() {
+	t := suite.T()
+	// 1. setup pahse
 	sampleUser := models.User{
 		ID:        uuid.NewString(),
 		Email:     "kellasw@gmail.com",
@@ -52,11 +73,10 @@ func Test_CreateUser(t *testing.T) {
 		UpdatedAt: time.Now(),
 	}
 
-	userRepo := newUserRepository(dbStore)
-	err = userRepo.Create(ctx, &sampleUser)
+	err := suite.userRepo.Create(suite.ctx, &sampleUser)
 	assert.NoError(t, err)
 
-	resp, err := userRepo.GetByEmail(ctx, sampleUser.Email)
+	resp, err := suite.userRepo.GetByEmail(suite.ctx, sampleUser.Email)
 	assert.NoError(t, err)
 
 	assert.Equal(t, sampleUser.ID, resp.ID)
@@ -66,25 +86,26 @@ func Test_CreateUser(t *testing.T) {
 	assert.Equal(t, sampleUser.Password, resp.Password)
 	assert.Equal(t, sampleUser.IsAdmin, resp.IsAdmin)
 
-	err = deleteUser(dbStore, sampleUser.ID)
+	err = deleteUser(suite.dbStore, sampleUser.ID)
 	require.NoError(t, err)
 }
 
-func Test_EmailExist(t *testing.T) {
+func (suite *UserRepoTestSuite) Test_EmailExist() {
+	t := suite.T()
 	// 1. setup pahse
 	// 1.1 setup db connectiom
-	err := godotenv.Load("../../../.env")
-	require.NoError(t, err)
-	testDBUrl := os.Getenv("TEST_DB_URL")
-	ctx := context.Background()
-	dbStore, err := db.ConnectDatabase(testDBUrl)
-	require.NoError(t, err)
+	// err := godotenv.Load("../../../.env")
+	// require.NoError(t, err)
+	// testDBUrl := os.Getenv("TEST_DB_URL")
+	// ctx := context.Background()
+	// dbStore, err := db.ConnectDatabase(testDBUrl)
+	// require.NoError(t, err)
 
-	defer db.CloseDatabase(dbStore)
+	// defer db.CloseDatabase(dbStore)
 
-	migrator := migration.New(dbStore)
-	err = migrator.Up()
-	assert.NoError(t, err)
+	// migrator := migration.New(dbStore)
+	// err = migrator.Up()
+	// assert.NoError(t, err)
 
 	sampleUser := models.User{
 		ID:        uuid.NewString(),
@@ -97,16 +118,16 @@ func Test_EmailExist(t *testing.T) {
 		UpdatedAt: time.Now(),
 	}
 
-	userRepo := newUserRepository(dbStore)
-	err = userRepo.Create(ctx, &sampleUser)
+	// userRepo := newUserRepository(dbStore)
+	err := suite.userRepo.Create(suite.ctx, &sampleUser)
 	assert.NoError(t, err)
 
-	resp, err := userRepo.EmailExists(ctx, sampleUser.Email)
+	resp, err := suite.userRepo.EmailExists(suite.ctx, sampleUser.Email)
 	assert.NoError(t, err)
 
 	assert.Equal(t, resp, true)
 
-	err = deleteUser(dbStore, sampleUser.ID)
+	err = deleteUser(suite.dbStore, sampleUser.ID)
 	require.NoError(t, err)
 }
 
@@ -114,4 +135,8 @@ func deleteUser(db *sql.DB, id string) error {
 	query := "DELETE FROM users WHERE id = $1"
 	_, err := db.Exec(query, id)
 	return err
+}
+
+func TestUserRepoTestSuite(t *testing.T) {
+	suite.Run(t, new(UserRepoTestSuite))
 }
